15629cc722727b7280fa1277000fc53e7fa9300f
Thuyen <thuyendangminh11a221@gmail.com>
2026-06-29 22:45:06 +0700

    a

---
+++ backend/config/db.js
@@ -0,0 +1,33 @@
+const sql = require("mssql");
+require("dotenv").config();
+
+const config = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
+    port: Number(process.env.DB_PORT),
+
+    options: {
+        trustServerCertificate: true,
+        encrypt: false,
+        enableArithAbort: true
+    }
+};
+
+async function connectDB() {
+    try {
+        console.log(config);
+        const pool = await sql.connect(config);
+        console.log("✅ Connected to SQL Server");
+        return pool;
+    } catch (err) {
+    console.error("❌ Lỗi kết nối SQL:");
+    console.error(err.message);
+}
+}
+
+module.exports = {
+    sql,
+    connectDB
+};
---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const { connectDB } = require("./config/db");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Kết nối Database
+connectDB();
+
+// Test API
+app.get("/", (req, res) => {
+    res.json({
+        message: "🚀 Server đang chạy thành công!"
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server đang chạy tại: http://localhost:${PORT}`);
+});
